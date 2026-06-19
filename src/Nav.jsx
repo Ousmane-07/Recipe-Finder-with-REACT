40ca229c22360fc20d55f75f866313289c8a7774
Ousmane <ousmanegitgit@gmail.com>
2026-06-19 13:58:08 -0400

    Navigation component complete

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,11 +1,21 @@
-
-
+import logo from './images/logo.svg'
+import { Link } from 'react-router-dom'
 function Nav() { 
 
 
     return ( 
         <>
-        hello
+        <nav className="nav-wrapper">
+            <div className="nav-container">
+            <img src={logo}></img>
+            <div className="nav-links">
+                <Link to="/" className="nav-text">Home</Link>
+                <Link to="/about" className="nav-text">About</Link>
+                <Link to="/recipes" className="nav-text">Recipes</Link>
+            </div>
+                <Link to="/recipes" className="browse-btn btn">Browse recipes</Link>
+            </div>
+        </nav>
         </>
     )
 }
