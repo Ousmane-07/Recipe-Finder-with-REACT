8f786bd46031b6389850aef2d024271c28e94b57
Ousmane <ousmanegitgit@gmail.com>
2026-06-22 16:56:57 -0400

    Added fake data for recipe finder

---
+++ src/data.jsx
@@ -0,0 +1,68 @@
+import mediterranean from './images/mediterranean-chickpea-salad-small.webp'
+import avocadotoast from './images/avocado-tomato-wholegrain-toast-small.webp'
+import salmon from './images/salmon-asparagus-small.webp'
+import quinoa from './images/quinoa-veggie-bowl-small.webp'
+import sweetpotato from './images/sweet-potato-tacos-small.webp'
+import greekyogurt from './images/greek-yogurt-small.webp'
+import lentil from './images/lentil-soup-small.webp'
+import bananapancakes from './images/banana-pancakes-small.webp'
+
+export const data = [
+    {
+        id: 1,
+        name: "Mediterranean Chickpea Salad",
+        image: mediterranean, 
+        prepTime: 10,
+        cookTime: 0
+    },
+        {
+        id: 2,
+        name: "Avocado & Tomato Wholegrain Toast",
+        image: avocadotoast, 
+        prepTime: 5,
+        cookTime: 5
+    },
+        {
+        id: 3,
+        name: "One Pan Lemon Garlic Salmon with Asparagus",
+        image: salmon, 
+        prepTime: 5,
+        cookTime: 12
+    },
+        {
+        id: 4,
+        name: "Quinoa Veggie Power Bowl",
+        image: quinoa, 
+        prepTime: 10,
+        cookTime: 15
+    },
+        {
+        id: 5,
+        name: "Sweet Potato Black Bean Tacos",
+        image: sweetpotato, 
+        prepTime: 10,
+        cookTime: 15
+    },
+        {
+        id: 6,
+        name: "Greek Yogurt Berry Parfait",
+        image: greekyogurt, 
+        prepTime: 5,
+        cookTime: 0
+    },
+        {
+        id: 7,
+        name: "Lentil & Spinach Soup",
+        image: lentil, 
+        prepTime: 10,
+        cookTime: 20
+    },
+        {
+        id: 8,
+        name: "Banana Oat Pancakes",
+        image: bananapancakes, 
+        prepTime: 5,
+        cookTime: 10
+    }
+]
+
